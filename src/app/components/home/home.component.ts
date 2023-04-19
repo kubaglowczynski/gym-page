e8014eddeb9b6447e153c2b589751c81b2bbc399
kubaglowczynski <114954433+kubaglowczynski@users.noreply.github.com>
2023-04-19 23:53:26 +0200

    home component gallery update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  styleCards0 = {display : 'block'};
+  styleCards1 = {display : 'none'};
+  styleCards2 = {display : 'none'};
 
+  goingRight(){
+    if(this.styleCards0.display === 'block'){
+      this.styleCards0 = {display : 'none'}
+      this.styleCards1 = {display : 'block'}
+      this.styleCards2 = {display : 'none'}
+      console.log("w prawo")
+    }
+    else if(this.styleCards1.display === 'block'){
+      this.styleCards0 = {display : 'none'}
+      this.styleCards1 = {display : 'none'}
+      this.styleCards2 = {display : 'block'}
+      console.log("w prawo2")
+    }
+    else if(this.styleCards2.display === 'block'){
+      this.styleCards0 = {display : 'block'}
+      this.styleCards1 = {display : 'none'}
+      this.styleCards2 = {display : 'none'}
+      console.log("w prawo3")
+    }
+  }
+
+  goingLeft(){
+    if(this.styleCards0.display === 'block'){
+      this.styleCards0 = {display : 'none'}
+      this.styleCards1 = {display : 'none'}
+      this.styleCards2 = {display : 'block'}
+      console.log("w lewo")
+    }
+    else if(this.styleCards1.display === 'block'){
+      this.styleCards0 = {display : 'block'}
+      this.styleCards1 = {display : 'none'}
+      this.styleCards2 = {display : 'none'}
+      console.log("w lewo2")
+    }
+    else if(this.styleCards2.display === 'block'){
+      this.styleCards0 = {display : 'none'}
+      this.styleCards1 = {display : 'block'}
+      this.styleCards2 = {display : 'none'}
+      console.log("w lewo 3")
+    }
+  }
 }
