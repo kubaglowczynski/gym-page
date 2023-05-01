b21f0924957919744df285d21b5b6ceb2d201ed7
kubaglowczynski <114954433+kubaglowczynski@users.noreply.github.com>
2023-05-02 01:30:27 +0200

    saturday schedule update

--- src/app/components/schedule/saturday/saturday.component.ts
+++ src/app/components/schedule/saturday/saturday.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['../../../app.component.scss']
 })
 export class SaturdayComponent {
+    footballIsExpired:boolean = false;
+    currentDayOfWeek: number = new Date().getDay();
+    nowHour: Date = new Date();
+    currentHour: number = this.nowHour.getHours();
 
+    addDivWithClass(className: string) {
+    const div = document.createElement("div");
+    div.textContent = "expired";
+    div.classList.add(className);
+    document.body.appendChild(div);
+  }
+
+  constructor(){
+    if(this.currentDayOfWeek === 2 && this.currentHour >= 9){
+      this.footballIsExpired = true;
+    }
+
+    if(this.footballIsExpired === true){
+      this.addDivWithClass("lesson_expired");
+      }
+    }
 }
