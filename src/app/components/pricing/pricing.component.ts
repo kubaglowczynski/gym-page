a48a06285eaaf2047024dea20f0d162dd805c249
kubaglowczynski <114954433+kubaglowczynski@users.noreply.github.com>
2023-05-04 16:56:17 +0200

    pricing component update

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./pricing.component.scss']
 })
 export class PricingComponent {
+  firstAnswer: boolean = false;
+  secondAnswer: boolean = false;
+  thirdAnswer: boolean = false;
+  fourthAnswer: boolean = false;
 
+  toggleFirstAnswer(){
+    if(this.firstAnswer === false){
+      this.firstAnswer = true;
+    }
+    else{
+      this.firstAnswer = false;
+    }
+  }
+
+  toggleSecondAnswer(){
+    if(this.secondAnswer === false){
+      this.secondAnswer = true;
+    }
+    else{
+      this.secondAnswer = false;
+    }
+  }
+
+  toggleThirdAnswer(){
+    if(this.thirdAnswer === false){
+      this.thirdAnswer = true;
+    }
+    else{
+      this.thirdAnswer = false;
+    }
+  }
+
+  toggleFourthAnswer(){
+    if(this.fourthAnswer === false){
+      this.fourthAnswer = true;
+    }
+    else{
+      this.fourthAnswer = false;
+    }
+  }
 }
