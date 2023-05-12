c94d9e0cbf5344fd4fbd9dac2b8a83f253d099c5
kubaglowczynski <glowczynski.jak@gmail.com>
2023-05-12 22:39:07 +0200

    contact component update

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements AfterViewInit{
+  isLoading = true;
 
+  constructor() {}
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 500);
+  }
 }
